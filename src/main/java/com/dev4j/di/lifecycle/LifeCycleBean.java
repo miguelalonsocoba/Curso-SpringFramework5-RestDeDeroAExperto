ab6021993c173f383b3dc5f2549d6334e136651c
mac <miguetheking53@gmail.com>
2022-03-04 09:45:15 -0600

    Callbaks @PostConstruct and @PreDestroy

--- src/main/java/com/dev4j/di/lifecycle/LifeCycleBean.java
+++ src/main/java/com/dev4j/di/lifecycle/LifeCycleBean.java
@@ -3,6 +3,9 @@
  */
 package com.dev4j.di.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
@@ -19,8 +22,17 @@ public class LifeCycleBean implements BeanNameAware{
 	
 	@Override
 	public void setBeanName(String name) {
-		LOGGER.info("Bean name {}", name);
-		
+		LOGGER.info("Bean name aware {}", name);
+	}
+	
+	@PostConstruct
+	public void init() {
+		LOGGER.info("Post Contruct.");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		LOGGER.info("Pre destroy");
 	}
 
 }
